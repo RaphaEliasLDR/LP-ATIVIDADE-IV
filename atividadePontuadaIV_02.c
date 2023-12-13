0878aad3e480ac0d6b5a4ebd5e677e29411c284b
Raphael Elias <143661944+RaphaEliasLDR@users.noreply.github.com>
2023-12-13 19:42:11 -0300

    Update atividadePontuadaIV_02.c

--- atividadePontuadaIV_02.c
+++ atividadePontuadaIV_02.c
@@ -3,77 +3,75 @@
 #include <string.h>
 #include <locale.h>
 
-#define TAM 3
-
-
+#define TAM 5
 
 
 struct dados_aluno{
 	char nome [200];
 	char dataDeNasc [200];
-	int notas [2];
+	int notas [TAM][2];
 	float media;
 };
 
 
-float calcularMedia (struct dados_aluno aluno[]){
-	int i;
-	int j;	
+void mostrarMedia (struct dados_aluno aluno[]){
+	int i;	
 	
 	for (i = 0; i < TAM; i++){
-	
-	printf ("\nNome do aluno(a): %s", aluno[i].nome);
-	printf ("\nData de nascimento: %s", aluno[i].dataDeNasc);
-	for (j = 0; j < 2; j++){
-		printf ("\n%dº nota: %d", j+1, aluno[i].notas[j]);
-	}
-	printf ("\nMédia: %.2f", aluno[i].media);
-}
+		printf ("\nNome do aluno(a): %s", aluno[i].nome);
+		printf ("\nMÃ©dia: %.2f", aluno[i].media);
+	}	
 }
 
+/*
+void resultadoAprovacao (struct dados_aluno aluno[], char* resultadoDoAluno){
+	int i;
+	
+	for (i = 0; i < TAM; i++){
+		if (strcmp (aluno[i].nome, resultadoDoAluno)==0){
+		printf ("\nNome: %s", aluno[i].nome);
+		return;
+	}
+	if (aluno[i].media >= 7){
+				printf ("Resultado: APROVADO!!");
+			}else {
+				printf ("Resultado: REPROVADO!!");
+			}
+	}
+	
+}
+*/
 
 int main (){
 	setlocale (LC_ALL, "portuguese");
+	
 	int i;
 	int j;
-	int somaNotas = 0;
-	
+	int somaNotas;
+	char resultadoDoAluno;
 	
 	struct dados_aluno aluno[TAM];
 	
 	for (i = 0; i < TAM; i++){
+		
 		printf ("\nNome do aluno(a): ");
 		gets (aluno[i].nome);
 		printf ("\nData de nascimento: ");
 		gets(aluno[i].dataDeNasc);
+		
 		for (j = 0; j < 2; j++){
-			printf ("\n%dº nota: ", j+1);
-			scanf ("%d", aluno[i].notas[j]);
-			somaNotas += aluno[i].notas[j];	
+			printf ("\n%dÂº nota: ", j+1);
+			scanf ("%f", &aluno[i].notas[j]);
+			somaNotas += aluno[i].notas[j];
 		}
-		aluno[i].media += somaNotas / j; 
-		fflush (stdin);
+		
+		aluno[i].media = somaNotas / j;
 		somaNotas = 0;
+		fflush(stdin);
 	}
-
-
-calcularMedia(aluno);
+	
+	
+	mostrarMedia(aluno);
 
 return 0;	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
